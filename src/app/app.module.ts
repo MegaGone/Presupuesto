00b7ecf96a48f1219800ec5ffec77fb47e172424
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2020-12-01 17:58:13 -0600

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './controllers/home/home.component';
 import { LoginComponent } from './controllers/login/login.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SettingsComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,

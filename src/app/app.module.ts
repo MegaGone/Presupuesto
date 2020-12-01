aab9ffdc443d0206a58e4cc592636c894d38bc3d
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2020-12-01 17:59:15 -0600

    delete bad controllers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './controllers/home/home.component';
-import { LoginComponent } from './controllers/login/login.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    LoginComponent,
     SettingsComponent,
     ErrorComponent
   ],

a9714a0b9a1a4cfe0d58c4ae4dddbefae25328a8
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2020-12-01 22:00:31 -0600

    Start with login ui

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,7 +7,7 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { ErrorComponent } from './components/error/error.component';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent},
+    { path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent},
     { path: 'home', component: HomeComponent},
     { path: 'settings', component: SettingsComponent},

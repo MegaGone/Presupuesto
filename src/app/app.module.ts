87966f50873c6c7f3e6aa7e14b4a2cd0863b4341
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2020-12-01 18:12:06 -0600

    Create routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { ErrorComponent } from './components/error/error.component';
+import { appRountingProviders, routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HomeComponent,
     SettingsComponent,
     ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [ appRountingProviders ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,18 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'settings', component: SettingsComponent},
+    { path: 'error', component: ErrorComponent}
+];
+
+export const appRountingProviders: any[] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);

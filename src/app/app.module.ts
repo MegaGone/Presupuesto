16c814cf3cfd662210a458960114f9c1419d485f
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2020-12-04 19:31:06 -0600

    login form validate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,9 @@ import { WelcomeComponent } from './components/welcome/welcome.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    routing
+    routing,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [ appRountingProviders ],
   bootstrap: [AppComponent]
